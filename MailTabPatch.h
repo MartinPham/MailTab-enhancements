e787c7f7507d6fac072d5cd2cc7907617c2aed83
Martin Pham <tuyentq2009@gmail.com>
2015-05-17 18:24:40 +0000

    init

---
+++ MailTabPatch.h
@@ -0,0 +1,15 @@
+//
+//  MailTabPatch.h
+//  MailTabPatch
+//
+//  Created by Martin Pham.
+//  All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MailTabPatch : NSObject
+
+-(void)PATCH_webView:(id)arg1 didFinishLoadForFrame:(id)arg2;
+
+@end
